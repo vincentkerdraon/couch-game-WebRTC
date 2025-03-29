71c1bdbe773c0db5acaa36e48aac510de0729f60
Vincent KERDRAON <vincent.kerdraon@gmail.com>
2025-03-29 06:05:27 -0600

    add Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterModule]
+  imports: [RouterModule, CommonModule]
 })
 export class AppComponent {
+  isCollapsed = true;
 }
